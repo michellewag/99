8c451bb781ce6fb23a3f3632f4f81ff10cbdd3dc
Michelle Wagner <mewag13@yahoo.com>
2019-05-10 00:12:52 +0000

    testing

--- 99bottles.js
+++ 99bottles.js
@@ -1,1 +1,11 @@
 //Michelle
+//start with a variable
+var numberOfBottle = 99;
+var text = ""
+
+
+while(numberOfBottle > 0) {
+    text = "The number is " + numberOfBottle;
+    console.log(text);
+    numberOfBottle--;
+}
